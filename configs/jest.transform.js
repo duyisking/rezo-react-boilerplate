301ee4b6edec1e5a76f28c89c92fad6cb638a376
duyisking <duyisking1060@gmail.com>
2019-06-28 00:09:36 +0700

    Remove configs

--- configs/jest.transform.js
+++
@@ -1,3 +0,0 @@
-const config = require('../config');
-
-module.exports = require('babel-jest').createTransformer(config.babelrc);
--- configs/scripts/node.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const { fork } = require('child_process');
-const constants = require('../constants');
-
-fork(path.resolve(constants.DIST_DIR, 'index.js'));
